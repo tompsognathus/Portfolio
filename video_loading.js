aea457c53ca0851dab530425d155d240c873cff9
tompsognathus <tompsognathus@gmail.com>
2023-02-13 01:08:06 +0000

    trying to fix autoplay

--- video_loading.js
+++ video_loading.js
@@ -1,24 +1,21 @@
-// Autoplay videos with id video1, video2, video3, video4 once they are loaded
-
-var video1 = document.getElementById('video1');
-var video2 = document.getElementById('video2');
-var video3 = document.getElementById('video3');
-var video4 = document.getElementById('video4');
-
-video1.addEventListener('loadeddata', function() {
-    video1.play();
-})
-
-video2.addEventListener('loadeddata', function() {
-    video2.play();
-})
-
-video3.addEventListener('loadeddata', function() {
-    video3.play();
-})
-
-video4.addEventListener('loadeddata', function() {
-    video4.play();
-})
+document.getElementById("video1").addEventListener("load", function () {
+    console.log("Video 1 loaded successfully");
+});
+document.getElementById("video1").src = "https://www.youtube.com/embed/csFna2PUTug?rel=0&amp;modestbranding=1&amp;controls=0&amp;autoplay=1&amp;mute=1&amp;loop=1&amp;playlist=csFna2PUTug";
 
 
+document.getElementById("video2").addEventListener("load", function () {
+    console.log("Video 2 loaded successfully");
+});
+document.getElementById("video2").src = "https://www.youtube.com/embed/5XVa716f2Xk?rel=0&amp;modestbranding=1&amp;controls=0&amp;autoplay=1&amp;mute=1&amp;loop=1&amp;playlist=5XVa716f2Xk";
+
+document.getElementById("video3").addEventListener("load", function () {
+    console.log("Video 3 loaded successfully");
+});
+document.getElementById("video3").src = "https://www.youtube.com/embed/SqUaykEyNgo?rel=0&amp;modestbranding=1&amp;controls=0&amp;autoplay=1&amp;mute=1&amp;loop=1&amp;playlist=SqUaykEyNgo";
+
+document.getElementById("video4").addEventListener("load", function () {
+    console.log("Video 4 loaded successfully");
+});
+
+document.getElementById("video4").src = "https://www.youtube.com/embed/BavPRveYnpg?rel=0&amp;modestbranding=1&amp;controls=0&amp;autoplay=1&amp;mute=1&amp;loop=1&amp;playlist=BavPRveYnpg";
