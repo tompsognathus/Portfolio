f389f2c8adf8b4aec6f1fedd16d820ba33c40804
tompsognathus <tompsognathus@gmail.com>
2023-02-13 02:14:45 +0000

    changed video order back

--- video_loading.js
+++ video_loading.js
@@ -1,20 +1,21 @@
 document.getElementById("video1").addEventListener("load", function () {
     console.log("Video 1 loaded successfully");
 });
-document.getElementById("video1").src = "https://www.youtube.com/embed/5XVa716f2Xk?rel=0&amp;modestbranding=1&amp;controls=0&amp;autoplay=1&amp;mute=1&amp;loop=1&amp;playlist=5XVa716f2Xk";
+document.getElementById("video1").src = "https://www.youtube.com/embed/csFna2PUTug?rel=0&amp;modestbranding=1&amp;controls=0&amp;autoplay=1&amp;mute=1&amp;loop=1&amp;playlist=csFna2PUTug";
+
 
 document.getElementById("video2").addEventListener("load", function () {
     console.log("Video 2 loaded successfully");
 });
-document.getElementById("video2").src = "https://www.youtube.com/embed/SqUaykEyNgo?rel=0&amp;modestbranding=1&amp;controls=0&amp;autoplay=1&amp;mute=1&amp;loop=1&amp;playlist=SqUaykEyNgo";
+document.getElementById("video2").src = "https://www.youtube.com/embed/5XVa716f2Xk?rel=0&amp;modestbranding=1&amp;controls=0&amp;autoplay=1&amp;mute=1&amp;loop=1&amp;playlist=5XVa716f2Xk";
 
 document.getElementById("video3").addEventListener("load", function () {
     console.log("Video 3 loaded successfully");
 });
-
-document.getElementById("video3").src = "https://www.youtube.com/embed/BavPRveYnpg?rel=0&amp;modestbranding=1&amp;controls=0&amp;autoplay=1&amp;mute=1&amp;loop=1&amp;playlist=BavPRveYnpg";
+document.getElementById("video3").src = "https://www.youtube.com/embed/SqUaykEyNgo?rel=0&amp;modestbranding=1&amp;controls=0&amp;autoplay=1&amp;mute=1&amp;loop=1&amp;playlist=SqUaykEyNgo";
 
 document.getElementById("video4").addEventListener("load", function () {
     console.log("Video 4 loaded successfully");
 });
-document.getElementById("video4").src = "https://www.youtube.com/embed/csFna2PUTug?rel=0&amp;modestbranding=1&amp;controls=0&amp;autoplay=1&amp;mute=1&amp;loop=1&amp;playlist=csFna2PUTug";
+
+document.getElementById("video4").src = "https://www.youtube.com/embed/BavPRveYnpg?rel=0&amp;modestbranding=1&amp;controls=0&amp;autoplay=1&amp;mute=1&amp;loop=1&amp;playlist=BavPRveYnpg";
